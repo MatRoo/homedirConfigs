ad257b7155d37e03a2ae18d7e9651ff3ddc0d974
Mattijs Roozeboom <contact@mattijsroozeboom.nl>
2013-08-05 20:42:08 +0200

    - added .gitignore - added weechat config (untested change of hotkeys - added .vim folder - added extip.sh (awesomewm stuff) - added homesetup.sh (setup multimonitor)

--- cplusplusfiddling/acceleratedcplusplus/exercise_0_4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	cout << "#include<iostream>" << endl << "using namespace std;" << endl << "int main()" << endl << "{" << endl << "\t cout << \"Hello world\" << endl;" << endl << "\t return 0;" << endl << "}" << endl;
-	return 0;
-}
---
+++ homesetup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDM1-0 --pos 0x0 --output LVDS-0 --pos 0x1920
